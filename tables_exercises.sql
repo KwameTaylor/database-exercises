e44d170074b18d841ec32eaf8649a4801ee6e299
Kwame Taylor <maxkwametaylor@gmail.com>
2020-07-20 14:21:09 -0500

    update exercise

--- tables_exercises.sql
+++ tables_exercises.sql
@@ -16,6 +16,6 @@ DESCRIBE employees;
 
 # Answer to number 8: employees, salaries
 
-# Answer to number 9: There doesn't seem to be any overlap in the variables in each table. Both tables have all NULL, require the fields to all be filled. The primary keys are both numbers, but the dept_no takes in a char, and the emp_no takes in an int.
+# Answer to number 9: There doesn't seem to be any overlap in the variables in each table. Both tables have all NULL, require the fields to all be filled. The primary keys are both numbers, but the dept_no takes in a char, and the emp_no takes in an int. They are both within the employees database, and there are other tables that seem related to each such as dept_emp and employees_with_departments.
 
 show create table dept_manager;
